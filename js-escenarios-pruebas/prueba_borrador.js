68304bd3547cd1a16f590b9b33d03f4b324544d3
OscarOtaloraBVC <oscar.otalora@nuamx.com>
2025-07-17 15:50:19 -0500

    ajuste libreria

--- js-escenarios-pruebas/prueba_borrador.js
+++ js-escenarios-pruebas/prueba_borrador.js
@@ -1,7 +1,6 @@
-import http from 'k6/http';
-import { sleep } from 'k6';
-import exec from 'k6/execution';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+//import http from 'k6/http';
+import { check, sleep } from 'k6';
+import exec from 'k6/x/exec';
 
 
 const HARBOR_URL = 'test-nuam-registry.coffeesoft.org';
