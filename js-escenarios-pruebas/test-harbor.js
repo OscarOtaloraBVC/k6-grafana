156c8ea8f5502636e898b2eb981b5648efec3fa3
OscarOtaloraBVC <oscar.otalora@nuamx.com>
2025-07-17 14:37:29 -0500

    depuracion

--- js-escenarios-pruebas/test-harbor.js
+++ js-escenarios-pruebas/test-harbor.js
@@ -1,26 +1,26 @@
 import { check, sleep } from 'k6';
 import exec from 'k6/x/exec';
 //import { Cmd } from 'k6/x/cmd';
-
+ 
 const HARBOR_URL = 'test-nuam-registry.coffeesoft.org';
 const PROJECT_NAME = 'test-devops';
 const IMAGE_NAME = 'ubuntu';
 const image_tag_prefix = 'xk6';
-
-
+ 
+ 
 export const options = {
-    vus: 20,
+    vus: 1,
     duration: '15s',
-
+ 
 };
-
+ 
 export default function () {
     const uniqueTag = image_tag_prefix + '-' + new Date().getTime();
-    const fullImageName =  HARBOR_URL+'/' +PROJECT_NAME + '/' + IMAGE_NAME + ':' + uniqueTag;
+    const fullImageName =  HARBOR_URL+'/' +PROJECT_NAME +'/ubuntu/'+new Date().getTime() + '/' + IMAGE_NAME + ':' + uniqueTag;
     const sourceImage = IMAGE_NAME + ':latest';
-
+ 
     console.log(`Pushing image: ${fullImageName} from source: ${sourceImage}`);
-
+ 
     try {
         console.log(`Tagging image: ${fullImageName} from source: ${sourceImage}`);
         exec.command('docker', ['tag', sourceImage, fullImageName]);
@@ -28,7 +28,7 @@ export default function () {
         console.error(`Error tagging image: ${error}`);
         check(false,{ 'exception during docker push': false });
     }
-
+ 
     try {
         console.log(`Pushing image: ${fullImageName} to Harbor`);
         exec.command('docker', ['push', fullImageName]);
@@ -36,6 +36,6 @@ export default function () {
         console.error(`Error pushing image: ${error}`);
         check(false,{ 'exception during docker push': false });
     }
-
+ 
     sleep(5); // Simulate some processing time
 }
