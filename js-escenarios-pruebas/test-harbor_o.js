6de6f4334d430355630d824e9e1c0b3b22a41e00
OscarOtaloraBVC <oscar.otalora@nuamx.com>
2025-07-17 14:16:25 -0500

    autenticacion

--- js-escenarios-pruebas/test-harbor_o.js
+++ js-escenarios-pruebas/test-harbor_o.js
@@ -7,14 +7,35 @@ const PROJECT_NAME = 'library';
 const IMAGE_NAME = 'ubuntu';
 const image_tag_prefix = 'latest';
  
+const HARBOR_USER = 'admin';
+const HARBOR_PASSWORD = 'r7Y5mQBwsM2lIj0';
  
 export const options = {
     vus: 1,
     duration: '15s',
  
 };
- 
+
+function dockerLogin() {
+    try {
+        console.log(`Authenticating with Harbor registry...`);
+        const cmd = `docker login ${HARBOR_URL} -u ${HARBOR_USER} -p ${HARBOR_PASSWORD}`;
+        exec.command('sh', ['-c', cmd]);
+        return true;
+    } catch (error) {
+        console.error(`Error during Docker login: ${error}`);
+        return false;
+    }
+}
+
 export default function () {
+
+    // Autenticación por cada usuario virtual (VU)
+    if (!dockerLogin()) {
+        check(false, { 'docker login failed': false });
+        return;
+    }
+        
     const uniqueTag = image_tag_prefix + '-' + new Date().getTime();
     const fullImageName =  HARBOR_URL+'/' +PROJECT_NAME +'/ubuntu/'+new Date().getTime() + '/' + IMAGE_NAME + ':' + uniqueTag;
     const sourceImage = IMAGE_NAME + ':latest';
