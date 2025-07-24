4757a100b308856db62dc9807aa76203a2e42cff
OscarOtaloraBVC <oscar.otalora@nuamx.com>
2025-07-24 16:06:53 -0500

    ajuste de tiempo  { duration: '5m', target: 15 }

--- js-escenarios-pruebas/Pruebas_15.js
+++ js-escenarios-pruebas/Pruebas_15.js
@@ -28,7 +28,7 @@ export const options = {
     //{ duration: '1m15s', target: 50 },
     //{ duration: '1m15s', target: 25 },
     //{ duration: '1m15s', target: 15 },
-    { duration: '1m15s', target: 15 }  
+    { duration: '5m', target: 15 }  
   ],
   thresholds: {
     http_req_duration: ['p(95)<500'],
