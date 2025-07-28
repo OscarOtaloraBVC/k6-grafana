3469241d698b2dbc5070183ff5c0a53ab8252c81
OscarOtaloraBVC <oscar.otalora@nuamx.com>
2025-07-28 15:03:15 -0500

    15 por minuto

--- js-escenarios-pruebas/script-ejecuciones/03_vault_secrets_test.js
+++ js-escenarios-pruebas/script-ejecuciones/03_vault_secrets_test.js
@@ -16,7 +16,7 @@ const errorCounter = new Counter('vault_errors');
 // ======================
 export let options = {
   stages: [
-    { duration: '5m', target: 100 }
+    { duration: '5m', target: 15 }
   ],
   thresholds: {
     //'http_req_duration': ['p(95)<500', 'p(99)<1000'], Propuesta original
